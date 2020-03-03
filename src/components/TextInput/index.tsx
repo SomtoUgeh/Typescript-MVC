1fc049d582f2939ef1c81dcee4fa465bc535b380
Somto M.Ugeh <smugeh@gmail.com>
2020-03-03 13:54:41 +0100

    Added header and todoInput field

---
+++ src/components/TextInput/index.tsx
@@ -0,0 +1,44 @@
+import React, { useState, useContext } from "react";
+import { Container } from "./style";
+import { v4 as uuidv4 } from "uuid";
+import { Todo, AppStateContext } from "contexts/AppState";
+
+const TextInput: React.FC = () => {
+  const [value, setValue] = useState<string>("");
+  const { todos, setTodos } = useContext(AppStateContext);
+
+  const addTodo = (e: React.KeyboardEvent<HTMLInputElement>): void => {
+    if (e.key === "Enter" && value.trim().length > 0) {
+      const todo: Todo = {
+        id: uuidv4(),
+        todo: value,
+        completed: false
+      };
+
+      setTodos(todos => [...todos, todo]);
+      setValue("");
+    }
+  };
+
+  return (
+    <Container>
+      <header className="header">
+        <h1>todos</h1>
+
+        <input
+          autoFocus
+          type="text"
+          value={value}
+          className="new-todo"
+          onKeyPress={e => addTodo(e)}
+          placeholder="What needs to be done?"
+          onChange={({ target: { value } }) => setValue(value)}
+        />
+      </header>
+
+      {JSON.stringify(todos, null, 2)}
+    </Container>
+  );
+};
+
+export default TextInput;
